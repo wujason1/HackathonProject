eebfb828181b73c2cc279b20769246aeb78c43e5
jason <jasondewulo@gmail.com>
2024-01-27 10:10:09 -0700

    first backend commit

---
+++ src/main/java/com/example/demo/HackathonProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HackathonProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HackathonProjectApplication.class, args);
+	}
+
+}
