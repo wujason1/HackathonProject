d9cc9213e60c7bc07d4a950237093b1e8a9b3bcf
Erynn <eryram98@gmail.com>
2024-02-12 10:53:19 -0800

    added dashboard page

---
+++ frontend/src/components/ChallengeBox.js
@@ -0,0 +1,56 @@
+import React, { useState, useRef } from 'react';
+import { Modal, Button, Form, InputGroup } from 'react-bootstrap';
+//import '../Challenges.css';
+
+function ChallengeBox({title, description, type}) {
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const [url, setUrl] = useState("");
+
+    return (
+        <div>
+            <Button className="challengeBox" size="lg" variant="light" onClick={handleShow}>
+            {title}
+            </Button>
+            <Modal
+                centered
+                show={show}
+                onHide={handleClose}
+            >
+            <Modal.Header closeButton>
+              <Modal.Title>{title}</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>{description}</Modal.Body>
+            <Modal.Footer>
+                {type === 'file' &&
+                  <Form.Group controlId="formFile" className="mb-3">
+                    <Form.Control type="file" />
+                  </Form.Group>
+                }
+                {type === 'url' &&
+                    <InputGroup className="mb-3">
+                        <Form.Control
+                          placeholder="Enter URL"
+                          aria-label="Recipient's username"
+                          aria-describedby="basic-addon2"
+                        />
+                        <Button variant="dark" id="button-addon2">
+                          Submit
+                        </Button>
+                    </InputGroup>
+                }
+                {type === null &&
+                    <Button onClick={handleClose}>
+                        Submit
+                    </Button>
+                }
+            </Modal.Footer>
+            </Modal>
+        </div>
+    );
+}
+
+export default ChallengeBox;
--- frontend/src/pages/Dashboard.js
+++ frontend/src/pages/Dashboard.js
@@ -1,9 +1,38 @@
+import React from 'react';
+import ChallengeBox from '../components/ChallengeBox.js';
+//import SideNav from './components/SideNav.js'
+//import './Challenges.css';
+
 const Dashboard = () => {
-  return (
-    <div>
-        <h1>Dashboard</h1>
-    </div>
-  )
-};
+        return(
+            <div>
+                <h1>Challenges</h1>
+                <div className='challengeDiv'>
+                <ChallengeBox
+                    title={"Update Resume"}
+                    description={"Something about why updating resume is necessary and maybe some resources to help " +
+                                    "user out."}
+                    type={"file"}
+                />
+                </div>
+                <div className='challengeDiv'>
+                <ChallengeBox
+                    title={"Coding Challenge"}
+                    description={"Something about why practicing coding challenges is crucial. Maybe throw up a " +
+                                    "statistic or something."}
+                    type={"url"}
+                />
+                </div>
+                <div className='challengeDiv'>
+                <ChallengeBox
+                    title={"Earn a LinkedIn Certificate"}
+                    description={"Talk about how this time can be used to review previous knowledge or pick up a new " +
+                                    "skill."}
+                    type={"url"}
+                />
+                </div>
+            </div>
+        );
+}
 
 export default Dashboard;
