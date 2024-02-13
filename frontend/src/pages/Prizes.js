9013678c0f08886a3c616d741ddfb2580695be59
Erynn <eryram98@gmail.com>
2024-02-12 16:24:32 -0800

    created PrizeCard component for prizes page

--- frontend/src/pages/Prizes.js
+++ frontend/src/pages/Prizes.js
@@ -1,48 +1,35 @@
-import { Card, Row, Col, Button, Container } from 'react-bootstrap';
+import { Row, Col, Container } from 'react-bootstrap';
+import PrizeCard from '../components/PrizeCard.js';
+import amazon from '../images/amazon.png';
+import visa from '../images/visa.png';
+import chipotle from '../images/chipotle.png';
 
-function Prizes() {
+const Prizes = () => {
+
   return (
   <Container fluid="sm">
         <Row>
             <Col>
-                <Card style={{ width: '18rem' }}>
-                  <Card.Img variant="top" src="holder.js/100px180" />
-                  <Card.Body>
-                    <Card.Title>Prize</Card.Title>
-                    <Card.Text>
-                      Some quick example text to build on the card title and make up the
-                      bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Redeem</Button>
-                  </Card.Body>
-                </Card>
-                </Col>
-                <Col>
-                <Card style={{ width: '18rem' }}>
-                  <Card.Img variant="top" src="holder.js/100px180" />
-                  <Card.Body>
-                    <Card.Title>Prize</Card.Title>
-                    <Card.Text>
-                      Some quick example text to build on the card title and make up the
-                      bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Redeem</Button>
-                  </Card.Body>
-                </Card>
-                </Col>
-                <Col>
-                <Card style={{ width: '18rem' }}>
-                  <Card.Img variant="top" src="holder.js/100px180" />
-                  <Card.Body>
-                    <Card.Title>Prize</Card.Title>
-                    <Card.Text>
-                      Some quick example text to build on the card title and make up the
-                      bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Redeem</Button>
-                  </Card.Body>
-                </Card>
-                </Col>
+                <PrizeCard
+                    active={false}
+                    title={"Amazon Gift Card"}
+                    image={amazon}
+                />
+            </Col>
+            <Col>
+                <PrizeCard
+                    active={true}
+                    title={"Pre-paid Visa"}
+                    image={visa}
+                />
+            </Col>
+            <Col>
+                <PrizeCard
+                    active={true}
+                    title={"Chipotle Meal"}
+                    image={chipotle}
+                />
+            </Col>
         </Row>
     </Container>
   );
