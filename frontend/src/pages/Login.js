693a25167e57bc6867462c07de55acc3e00c6235
Kira Sanford <32987244+kirasanford@users.noreply.github.com>
2024-02-22 16:02:10 -0700

    fix merge conflicts

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -27,7 +27,6 @@ const Login = ( ) => {
                 // Redirect to dashboard upon successful registration
                 login();
                 window.location.href = '/dashboard';
-                console.log('going to dashboard');
 
             } else {
                 // error message
